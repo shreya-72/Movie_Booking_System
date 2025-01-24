4d9c92dc4ca508887650a9c8bfc396cdc9122525
Shreya Naik <96948120+shreya-72@users.noreply.github.com>
2025-01-24 11:49:12 +0530

    Update test.py

--- test.py
+++ test.py
@@ -23,7 +23,7 @@ def test_insert_theaters():
   
   for data in data_list:
     response = requests.post(url, json=data)
-    response = requests.post(f"{url}/theater", json = data)
+    response = requests.post(f"{url}/entity", json = data)
     
     # Print the status code and response for each request
     print(f"Response Status Code: {response.status_code}")
@@ -40,7 +40,7 @@ def test_update_theaters():
     }
 
     try:
-        response = requests.put(f"{url}/theater", json=theater_data)
+        response = requests.put(f"{url}/entity", json=theater_data)
         
         # Check if the response status is successful
         if response.status_code == 200:
@@ -72,7 +72,7 @@ def test_delete_theaters():
   ]
 
   for data in data_list:
-    response = requests.delete(f"{url}/theater", json = data)
+    response = requests.delete(f"{url}/entity", json = data)
     
     # Print the status code and response for each request
     print(f"Response Status Code: {response.status_code}")
@@ -88,7 +88,7 @@ def test_display_theaters():
 
     try:
         # Send a GET request to the endpoint with query parameters
-        response = requests.get(f"{url}/theater", params=query_params)
+        response = requests.get(f"{url}/entity", params=query_params)
         
         # Check if the response status is successful
         if response.status_code == 200:
